d53e3cbd437afb4e318ff17682636d235a8303bb
Karan <karanbhagel333@gmail.com>
2024-03-16 21:27:29 +0530

    first commit

---
+++ JSProject/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using JSProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult _popupView(int Id)
+        {
+            TestModel model = new TestModel();
+            return PartialView(model);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SaveData(TestModel model)
+        {
+            return Json(true);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JSProject/Models/TestModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+
+namespace JSProject.Models
+{
+    public class TestModel
+    {
+        public TestModel()
+        {
+            this.Checkbox = new List<string>();
+        }
+        public int Id { get; set; }
+        public List<string> Checkbox { get; set; }
+
+        public IFormFile FileName { get; set; }
+
+
+       // [Required(ErrorMessage ="Role name is required")]
+        [LocalizedRequired("NameRequired")]
+        public string RoleName { get; set; }
+    }
+}
+
+
+public class LocalizedRequiredAttribute : RequiredAttribute
+{
+    private readonly string _resourceKey;
+
+    public LocalizedRequiredAttribute(string resourceKey)
+    {
+        _resourceKey = resourceKey;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        // Get the localized error message from the database using the resource key
+        string errorMessage = GetErrorMessageFromDatabase(_resourceKey);
+
+        // Replace the {0} placeholder with the name of the property
+        return string.Format(errorMessage, name);
+    }
+
+    private string GetErrorMessageFromDatabase(string resourceKey)
+    {
+        // TODO: Retrieve the localized error message from the database using the resource key
+        // You can use any method you prefer to retrieve the error message from the database
+        // This example assumes that you have a table called "ErrorMessages" with columns "ResourceKey" and "Message"
+        string connectionString = "your connection string here";
+        using (var connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            var command = new SqlCommand("SELECT Message FROM ErrorMessages WHERE ResourceKey = @ResourceKey", connection);
+            command.Parameters.AddWithValue("@ResourceKey", resourceKey);
+            return (string)command.ExecuteScalar();
+        }
+    }
+}
